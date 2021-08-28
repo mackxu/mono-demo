f3d2d2d8d52e9722f408f950e4bb313bfb3c9f98
xuyadong <xuyadong@sogou-inc.com>
2021-08-28 19:11:17 +0800

    pb change

--- packages/project-b/src/App.js
+++ packages/project-b/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { a } from '@momo/apis'
+import { a, b } from '@momo/apis'
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          a: {a}
+          a: {a}, b: { b }
         </p>
       </header>
     </div>
