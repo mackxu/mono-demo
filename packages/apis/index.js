725e88ad20ecae62f7c97f5ad70484505a330ad9
xuyadong <xuyadong@sogou-inc.com>
2021-08-28 21:48:59 +0800

    test publish

--- packages/apis/index.js
+++ packages/apis/index.js
@@ -1,2 +1,3 @@
 export const a = 2
 export const b = 3
+export const c = 4
--- packages/hooks/index.js
+++ packages/hooks/index.js
@@ -9,3 +9,7 @@ export function test2() {
 export function test3() {
   console.log('hooks test3');
 }
+
+export function test4() {
+  console.log('hooks test3');
+}
